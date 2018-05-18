2887d8e1885d326f7078926c8907ef59acd5b20b
DHBXinxin <461132172@qq.com>
2018-05-18 14:17:21 +0800

    AllocInit

--- AllocInit/AllocInit/ViewController.m
+++ AllocInit/AllocInit/ViewController.m
@@ -24,8 +24,14 @@
     //    将该新对象的 isa 成员变量指向它的类对象。
     //    将该新对象的所有其它成员变量的值设置成零。（根据成员变量类型，零有可能是指 nil 或 Nil 或 0.0）
     //对于isa的理解看本程序的图更能明显的理解
+    //init的作用
+    //为对象属性赋值
+    //alloc的作用
+//    内存管理的事情，设置 Retain Count。
+//    运行时自省的功能，设置 isa 变量。
+//    非逻辑性的初使化功能，设置所有成员变量为零。
+    //new只是alloc+init
 }
-
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
