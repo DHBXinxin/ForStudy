b839b69cc8e63cd1cb514d53ad14c83e42fdc4ea
李如欣 <461132172@qq.com>
2018-09-10 15:36:37 +0800

    0000

---
+++ ForRuntime/ForRuntime/PieceMain.m
@@ -0,0 +1,18 @@
+//
+//  PieceMain.m
+//  ForRuntime
+//
+//  Created by DHSD on 2018/8/20.
+//  Copyright © 2018年 DHSD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//碎片、小零碎
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
