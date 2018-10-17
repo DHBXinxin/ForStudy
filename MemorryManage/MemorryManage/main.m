0d27257d18e4375c064158e6e1c5964e309033c3
李如欣 <461132172@qq.com>
2018-10-17 08:43:35 +0800

    CStruct

---
+++ MemorryManage/MemorryManage/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  MemorryManage
+//
+//  Created by DHSD on 2018/10/16.
+//  Copyright © 2018年 DHSD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+//内存管理
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
