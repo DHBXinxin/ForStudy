9983183f5d35ca5573b6fe428b6ec0a439907540
李如欣 <461132172@qq.com>
2019-05-31 11:43:31 +0800

    CGBitmapContext

---
+++ ForCGBitmapContext/ForCGBitmapContext/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ForCGBitmapContext
+//
+//  Created by DHSD on 2019/5/31.
+//  Copyright © 2019 DHSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//为了理解文字的CGBitmapContext方法而写
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ForCGBitmapContext/ForCGBitmapContext/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ForCGBitmapContext
+//
+//  Created by DHSD on 2019/5/31.
+//  Copyright © 2019 DHSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
