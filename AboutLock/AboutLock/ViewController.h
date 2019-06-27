b88420bafa36d6c1cd04e74481a081c06423374a
李如欣 <461132172@qq.com>
2019-06-27 08:57:23 +0800

    AboutLock

---
+++ AboutLock/AboutLock/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AboutLock
+//
+//  Created by DHSD on 2019/6/27.
+//  Copyright © 2019 DHSD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ AboutLock/AboutLock/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  AboutLock
+//
+//  Created by DHSD on 2019/6/27.
+//  Copyright © 2019 DHSD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
